8f573893802b2a5b31f1bf24eecd62f9d624c674
Andrew Yu <andrewyu@D-69-91-185-206.dhcp4.washington.edu>
2015-10-08 19:12:36 -0700

    add files

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,100 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Derry Cheng on 10/8/15.
+//  Copyright © 2015 Derry Cheng. All rights reserved.
+
+//  This is a console program where users can do some simple or extended calculations.
+//  Users can do simple calculations (+,-,*,/,%) either on multiple or single lines.
+//  The syntax for multiple lines input is: "number". Return. "expression". Return. "number".
+//  The syntax for single line input is: "number expression number". Return.
+
+//  Users can calcualte extended calculations using the following functions:
+//  factorials use "fact", average use "avg", or to count the numbers use "count".
+//  Users can use these functions by following this syntax:
+//  "number number number...function".
+
+
+import Foundation
+
+print("Hello, welcome to the calculator!")
+func input() -> String {
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    let result = NSString(data: inputData,
+        encoding:NSUTF8StringEncoding) as! String
+    return
+        result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+}
+
+func convert(incoming:String) -> Int {
+    return NSNumberFormatter().numberFromString(incoming)!.integerValue
+}
+
+var play : String = "Y";
+while (play.lowercaseString == "y") {
+    print("Let me know what you want to calculate.");
+    let userInput : String = input()
+    let userInputArr = userInput.componentsSeparatedByString(" ")
+    var result : Int = -1;
+    var resultDouble : Double = -1.0;
+    var avgStatus : Int = 0;
+    
+    if (userInputArr.count != 1){
+        let expression = userInputArr [1]
+        if (expression == "+"){
+            result = convert(userInputArr[0]) + convert(userInputArr[2])
+        } else if (expression == "-"){
+            result = convert(userInputArr[0]) - convert(userInputArr[2])
+        } else if (expression == "*"){
+            result = convert(userInputArr[0]) * convert(userInputArr[2])
+        } else if (expression == "/"){
+            result = convert(userInputArr[0]) / convert(userInputArr[2])
+        } else if (expression == "%"){
+            result = convert(userInputArr[0]) % convert(userInputArr[2])
+        } else if (userInputArr[userInputArr.count-1] == "count"){
+            result = userInputArr.count-1;
+        } else if (userInputArr[userInputArr.count-1] == "avg"){
+            var total = 0.0;
+            for var i = 0; i <= userInputArr.count-2; i++ {
+                total += Double(convert(userInputArr[i]));
+            }
+            resultDouble = Double(total/(Double(userInputArr.count)-1.0));
+            avgStatus = 1;
+        } else if (userInputArr[userInputArr.count-1] == "fact"){
+            var total = 1;
+            for var i = 1; i <= convert(userInputArr[0]); i++ {
+                total *= i;
+            }
+            result = total;
+        }
+    } else {
+        let firstnum = convert(userInputArr[0])
+        let expression = input()
+        let secondnum = convert(input())
+        if(expression == "+"){
+            result = firstnum + secondnum
+        } else if(expression == "-"){
+            result = firstnum - secondnum
+        } else if(expression == "*"){
+            result = firstnum * secondnum
+        } else if(expression == "/"){
+            result = firstnum / secondnum
+        } else if(expression == "%"){
+            result = firstnum % secondnum
+        }
+    }
+    
+    
+    if (result >= 0){
+        print("Result: \(result)");
+    } else if(avgStatus == 1){
+        print("Result: \(resultDouble)");
+    } else {
+        print ("Invalid result");
+    }
+    print("");
+    print ("Calculate again?  Y/N")
+    play = input()
+}
